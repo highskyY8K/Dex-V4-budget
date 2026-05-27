3bff729950b35f8f836fb85a3feaf5674c1fe178
highskyY8K <highskyY8K@gmail.com>
2026-05-27 14:50:22 +0100

    Update Dex.lua

--- Dex.lua
+++ Dex.lua
@@ -6404,44 +6404,6 @@ return search]]
 			Lib.IconMap = (function()
 				local funcs = {}
 				local IconList = {
-					Old = {
-						MapId = 483448923, IconSize = 16, Witdh = 16, Height = 16,
-						Icons = {
-							["Accessory"] = 32; ["Accoutrement"] = 32; ["AdService"] = 73; ["Animation"] = 60; ["AnimationController"] = 60;
-							["AnimationTrack"] = 60; ["Animator"] = 60;["ArcHandles"] = 56; ["AssetService"] = 72; ["Attachment"] = 34;
-							["Backpack"] = 20; ["BadgeService"] = 75;["BallSocketConstraint"] = 89; ["BillboardGui"] = 64; ["BinaryStringValue"] = 4;
-							["BindableEvent"] = 67; ["BindableFunction"] = 66;["BlockMesh"] = 8; ["BloomEffect"] = 90; ["BlurEffect"] = 90;["BodyAngularVelocity"] = 14; ["BodyForce"] = 14;["BodyGyro"] = 14; ["BodyPosition"] = 14; ["BodyThrust"] = 14;
-							["BodyVelocity"] = 14; ["BoolValue"] = 4;["BoxHandleAdornment"] = 54; ["BrickColorValue"] = 4; ["Camera"] = 5;["CFrameValue"] = 4; ["CharacterMesh"] = 60; ["Chat"] = 33; ["ClickDetector"] = 41; ["CollectionService"] = 30;["Color3Value"] = 4; ["ColorCorrectionEffect"] = 90;["ConeHandleAdornment"] = 54; ["Configuration"] = 58;
-							["ContentProvider"] = 72;["ContextActionService"] = 41; ["CoreGui"] = 46;["CoreScript"] = 18; ["CornerWedgePart"] = 1;
-							["CustomEvent"] = 4;["CustomEventReceiver"] = 4; ["CylinderHandleAdornment"] = 54;["CylinderMesh"] = 8;
-							["CylindricalConstraint"] = 89;["Debris"] = 30; ["Decal"] = 7; ["Dialog"] = 62; ["DialogChoice"] = 63;
-							["DoubleConstrainedValue"] = 4;["Explosion"] = 36; ["FileMesh"] = 8; ["Fire"] = 61;["Flag"] = 38;
-							["FlagStand"] = 39; ["FloorWire"] = 4; ["Folder"] = 70; ["ForceField"] = 37; ["Frame"] = 48;
-							["GamePassService"] = 19; ["Glue"] = 34; ["GuiButton"] = 52; ["GuiMain"] = 47; ["GuiService"] = 47;["Handles"] = 53; ["HapticService"] = 84; ["Hat"] = 45; ["HingeConstraint"] = 89; ["Hint"] = 33;["HopperBin"] = 22; ["HttpService"] = 76; ["Humanoid"] = 9; ["ImageButton"] = 52;["ImageLabel"] = 49;
-							["InsertService"] = 72; ["IntConstrainedValue"] = 4; ["IntValue"] = 4;["JointInstance"] = 34;["JointsService"] = 34;
-							["Keyframe"] = 60; ["KeyframeSequence"] = 60; ["KeyframeSequenceProvider"] = 60; ["Lighting"] = 13;
-							["LineHandleAdornment"] = 54;["LocalScript"] = 18; ["LogService"] = 87;["MarketplaceService"] = 46; ["Message"] = 33;
-							["Model"] = 2; ["ModuleScript"] = 71; ["Motor"] = 34; ["Motor6D"] = 34;["MoveToConstraint"] = 89;
-							["NegateOperation"] = 78;["NetworkClient"] = 16;["NetworkReplicator"] = 29; ["NetworkServer"] = 15;
-							["NumberValue"] = 4; ["ObjectValue"] = 4; ["Pants"] = 44; ["ParallelRampPart"] = 1; ["Part"] = 1;["ParticleEmitter"] = 69;["PartPairLasso"] = 57; ["PathfindingService"] = 37; ["Platform"] = 35;
-							["Player"] = 12;["PlayerGui"] = 46;["Players"] = 21; ["PlayerScripts"] = 82; ["PointLight"] = 13;
-							["PointsService"] = 83; ["Pose"] = 60; ["PrismaticConstraint"] = 89; ["PrismPart"] = 1; ["PyramidPart"] = 1;
-							["RayValue"] = 4; ["ReflectionMetadata"] = 86; ["ReflectionMetadataCallbacks"] = 86; ["ReflectionMetadataClass"] = 86;
-							["ReflectionMetadataClasses"] = 86; ["ReflectionMetadataEnum"] = 86;["ReflectionMetadataEnumItem"] = 86;
-							["ReflectionMetadataEnums"] = 86;["ReflectionMetadataEvents"] = 86;["ReflectionMetadataFunctions"] = 86;["ReflectionMetadataMember"] = 86;["ReflectionMetadataProperties"] = 86; ["ReflectionMetadataYieldFunctions"] = 86;
-							["RemoteEvent"] = 80; ["RemoteFunction"] = 79;["ReplicatedFirst"] = 72;["ReplicatedStorage"] = 72;["RightAngleRampPart"] = 1;["RocketPropulsion"] = 14; ["RodConstraint"] = 89; ["RopeConstraint"] = 89;
-							["Rotate"] = 34;["RotateP"] = 34;["RotateV"] = 34; ["RunService"] = 66; ["ScreenGui"] = 47;
-							["Script"] = 6; ["ScrollingFrame"] = 48; ["Seat"] = 35; ["Selection"] = 55;["SelectionBox"] = 54;["SelectionPartLasso"] = 57; ["SelectionPointLasso"] = 57; ["SelectionSphere"] = 54; ["ServerScriptService"] = 0;
-							["ServerStorage"] = 74; ["Shirt"] = 43; ["ShirtGraphic"] = 40; ["SkateboardPlatform"] = 35; ["Sky"] = 28;
-							["SlidingBallConstraint"] = 89; ["Smoke"] = 59; ["Snap"] = 34; ["Sound"] = 11; ["SoundService"] = 31;
-							["Sparkles"] = 42;["SpawnLocation"] = 25;["SpecialMesh"] = 8; ["SphereHandleAdornment"] = 54;["SpotLight"] = 13; ["SpringConstraint"] = 89;["StarterCharacterScripts"] = 82; ["StarterGear"] = 20;
-							["StarterGui"] = 46;["StarterPack"] = 20; ["StarterPlayer"] = 88;["StarterPlayerScripts"] = 82;
-							["Status"] = 2; ["StringValue"] = 4; ["SunRaysEffect"] = 90; ["SurfaceGui"] = 64; ["SurfaceLight"] = 13;["SurfaceSelection"] = 55; ["Team"] = 24;["Teams"] = 23; ["TeleportService"] = 81; ["Terrain"] = 65;
-							["TerrainRegion"] = 65; ["TestService"] = 68; ["TextBox"] = 51;["TextButton"] = 51; ["TextLabel"] = 50;
-							["Texture"] = 10; ["TextureTrail"] = 4; ["Tool"] = 17; ["TouchTransmitter"] = 37; ["TrussPart"] = 1;["UnionOperation"] = 77; ["UserInputService"] = 84; ["Vector3Value"] = 4; ["VehicleSeat"] = 35;
-							["VelocityMotor"] = 34;["WedgePart"] = 1;["Weld"] = 34; ["Workspace"] = 19;
-						}
-					},
 					Vanilla3 = { 
 						MapId = 114851699900089, IconSize = 32, Witdh = 25, Height = 25,
 						Icons = {
@@ -6542,54 +6504,7 @@ return search]]
 							WireframeHandleAdornment = 604, Workspace = 605, WorldModel = 606, WorldRoot = 607, WrapLayer = 608, WrapTarget = 609
 						}
 					},
-					NewDark = {
-						MapId = 135148380892747, IconSize = 32, Witdh = 18, Height = 18,
-						Icons = {
-							Accessory = 1, Actor = 2, AdGui = 3, AdPortal = 4, AirController = 5, AlignOrientation = 6, AlignPosition = 7, AngularVelocity = 8,
-							Animation = 9, AnimationConstraint = 10, AnimationController = 11, AnimationFromVideoCreatorService = 12, Animator = 13, ArcHandles = 14,
-							Atmosphere = 15, Attachment = 16, AudioAnalyzer = 17, AudioChannelMixer = 18, AudioChannelSplitter = 19, AudioChorus = 20, AudioCompressor = 21,
-							AudioDeviceInput = 22, AudioDeviceOutput = 23, AudioDistortion = 24, AudioEcho = 25, AudioEmitter = 26, AudioEqualizer = 27, AudioFader = 28,
-							AudioFilter = 29, AudioFlanger = 30, AudioGate = 31, AudioLimiter = 32, AudioListener = 33, AudioPitchShifter = 34, AudioPlayer = 35,
-							AudioRecorder = 36, AudioReverb = 37, AudioTextToSpeech = 38, AuroraScript = 39, AvatarEditorService = 40, AvatarSettings = 41, Backpack = 42,
-							BallSocketConstraint = 43, BasePlate = 44, Beam = 45, BillboardGui = 46, BindableEvent = 47, BindableFunction = 48, BlockMesh = 49, BloomEffect = 50,
-							BlurEffect = 51, BodyAngularVelocity = 52, BodyColors = 53, BodyForce = 54, BodyGyro = 55, BodyPosition = 56, BodyThrust = 57, BodyVelocity = 58,
-							Bone = 59, BoolValue = 60, BoxHandleAdornment = 61, Breakpoint = 62, BrickColorValue = 63, BubbleChatConfiguration = 64, Buggaroo = 65, Camera = 66,
-							CanvasGroup = 67, CFrameValue = 68, ChannelTabsConfiguration = 69, CharacterControllerManager = 70, CharacterMesh = 71, Chat = 72,
-							ChatInputBarConfiguration = 73, ChatWindowConfiguration = 74, ChorusSoundEffect = 75, Class = 76, Cleanup = 77, ClickDetector = 78,
-							ClientReplicator = 79, ClimbController = 80, Clouds = 81, Color = 82, ColorCorrectionEffect = 83, CompressorSoundEffect = 84, ConeHandleAdornment = 85,
-							Configuration = 86, Constant = 87, Constructor = 88, Controller = 89, CoreGui = 90, CornerWedgePart = 91, CylinderHandleAdornment = 92,
-							CylindricalConstraint = 93, Decal = 94, DepthOfFieldEffect = 95, Dialog = 96, DialogChoice = 97, DistortionSoundEffect = 98, DragDetector = 99,
-							EchoSoundEffect = 100, EditableImage = 101, EditableMesh = 102, Enum = 103, EnumMember = 104, EqualizerSoundEffect = 105, Event = 106,
-							Explosion = 107, FaceControls = 108, Field = 109, File = 110, Fire = 111, FlangeSoundEffect = 112, Folder = 113, ForceField = 114, Frame = 115,
-							Function = 116, GameSettings = 117, GroundController = 118, Handles = 119, HapticEffect = 120, HapticService = 121, HeightmapImporterService = 122,
-							Highlight = 123, HingeConstraint = 124, Humanoid = 125, HumanoidDescription = 126, IKControl = 127, ImageButton = 128, ImageHandleAdornment = 129,
-							ImageLabel = 130, InputAction = 131, InputBinding = 132, InputContext = 133, Interface = 134, IntersectOperation = 135, Keyword = 136, Lighting = 137,
-							LinearVelocity = 138, LineForce = 139, LineHandleAdornment = 140, LocalFile = 141, LocalizationService = 142, LocalizationTable = 143, LocalScript = 144,
-							MaterialService = 145, MaterialVariant = 146, MemoryStoreService = 147, MeshPart = 148, Meshparts = 149, MessagingService = 150, Method = 151,
-							Model = 152, Modelgroups = 153, Module = 154, ModuleScript = 155, Motor6D = 156, NegateOperation = 157, NetworkClient = 158, NoCollisionConstraint = 159,
-							Operator = 160, PackageLink = 161, Pants = 162, Part = 163, ParticleEmitter = 164, Path2D = 165, PathfindingLink = 166, PathfindingModifier = 167,
-							PathfindingService = 168, PitchShiftSoundEffect = 169, Place = 170, Placeholder = 171, Plane = 172, PlaneConstraint = 173, Player = 174, Players = 175,
-							PluginGuiService = 176, PointLight = 177, PrismaticConstraint = 178, Property = 179, ProximityPrompt = 180, PublishService = 181, Reference = 182,
-							RemoteEvent = 183, RemoteFunction = 184, RenderingTest = 185, ReplicatedFirst = 186, ReplicatedScriptService = 187, ReplicatedStorage = 188,
-							ReverbSoundEffect = 189, RigidConstraint = 190, RobloxPluginGuiService = 191, RocketPropulsion = 192, RodConstraint = 193, RopeConstraint = 194,
-							Rotate = 195, ScreenGui = 196, Script = 197, ScrollingFrame = 198, Seat = 199, Selected_Workspace = 200, SelectionBox = 201, SelectionSphere = 202,
-							ServerScriptService = 203, ServerStorage = 204, Service = 205, Shirt = 206, ShirtGraphic = 207, SkinnedMeshPart = 208, Sky = 209, Smoke = 210,
-							Snap = 211, Snippet = 212, SocialService = 213, Sound = 214, SoundEffect = 215, SoundGroup = 216, SoundService = 217, Sparkles = 218,
-							SpawnLocation = 219, SpecialMesh = 220, SphereHandleAdornment = 221, SpotLight = 222, SpringConstraint = 223, StandalonePluginScripts = 224,
-							StarterCharacterScripts = 225, StarterGui = 226, StarterPack = 227, StarterPlayer = 228, StarterPlayerScripts = 229, Struct = 230, StyleDerive = 231,
-							StyleLink = 232, StyleRule = 233, StyleSheet = 234, SunRaysEffect = 235, SurfaceAppearance = 236, SurfaceGui = 237, SurfaceLight = 238,
-							SurfaceSelection = 239, SwimController = 240, TaskScheduler = 241, Team = 242, Teams = 243, Terrain = 244, TerrainDetail = 245, TestService = 246,
-							TextBox = 247, TextBoxService = 248, TextButton = 249, TextChannel = 250, TextChatCommand = 251, TextChatService = 252, TextLabel = 253, TextString = 254,
-							Texture = 255, Tool = 256, Torque = 257, TorsionSpringConstraint = 258, Trail = 259, TremoloSoundEffect = 260, TrussPart = 261, TypeParameter = 262,
-							UGCValidationService = 263, UIAspectRatioConstraint = 264, UICorner = 265, UIDragDetector = 266, UIFlexItem = 267, UIGradient = 268, UIGridLayout = 269,
-							UIListLayout = 270, UIPadding = 271, UIPageLayout = 272, UIScale = 273, UISizeConstraint = 274, UIStroke = 275, UITableLayout = 276,
-							UITextSizeConstraint = 277, UnionOperation = 278, Unit = 279, UniversalConstraint = 280, UnreliableRemoteEvent = 281, UpdateAvailable = 282,
-							UserService = 283, Value = 284, Variable = 285, VectorForce = 286, VehicleSeat = 287, VideoDisplay = 288, VideoFrame = 289, VideoPlayer = 290,
-							ViewportFrame = 291, VirtualUser = 292, VoiceChannel = 293, Voicechat = 294, VoiceChatService = 295, VRService = 296, WedgePart = 297, Weld = 298,
-							WeldConstraint = 299, Wire = 300, WireframeHandleAdornment = 301, Workspace = 302, WorldModel = 303, WrapDeformer = 304, WrapLayer = 305,
-							WrapTarget = 306, Color3Value = 284, IntValue = 284, NumberValue = 284, ObjectValue = 284, RayValue = 284, StringValue = 284, Vector3Value = 284
-						}
-					},
+					
 					NewLight = {
 						MapId = "", IconSize = 16, Witdh = 18, Height = 18,
 						Icons = {
@@ -6597,6 +6512,9 @@ return search]]
 						}
 					}
 				}
+				
+				IconList.Old = IconList.Vanilla3
+				IconList.NewDark = IconList.Vanilla3
 
 				if Settings.ClassIcon and IconList[Settings.ClassIcon] then
 					funcs.ExplorerIcons = {["MapId"] = IconList[Settings.ClassIcon].MapId,
@@ -16430,7 +16348,7 @@ DefaultSettings = (function()
 		},
 
 		RemoteBlockWriteAttribute = false,
-		ClassIcon = "NewDark",
+		ClassIcon = "Vanilla3",
 	}
 end)()
 
@@ -16576,7 +16494,7 @@ Main = (function()
 						writefile(path, decoded)
 					end
 				end
-				intro.SetProgress("Fetching Images", 0.6 + (i / 200))
+				intro.SetProgress("Fetching Images", 0.6 - (i / 200))
 			end)
 		end
 	end
